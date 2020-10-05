fe720c9e14e7090095dbb1904e0a6671b7d9b86c
Serhii Moroz <serhii.moroz@globalogic.com>
2020-10-05 22:01:20 +0300

    moved api response to json format

--- src/index.php
+++ src/index.php
@@ -6,10 +6,9 @@ $server = new Swoole\Http\Server("app", 9000, SWOOLE_BASE);
 
 $server->on("request", function (Swoole\Http\Request $request, Swoole\HTTP\Response $response) {
     $app = App\RunnerFactory::create();
-    $app->run();
 
-    $response->header("Content-Type", "text/plain");
-    $response->end($app->run());
+    $response->header("Content-Type", "application/json");
+    $response->end(json_encode($app->run()));
 });
 
 $server->start();
