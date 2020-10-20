e611ff578f20b82036949203bdace370003631eb
Serhii Moroz <serhii.moroz@globalogic.com>
2020-10-20 23:04:34 +0300

    docker: moved startup command to compose configs

--- docker/scripts/install.sh
+++ docker/scripts/install.sh
@@ -13,7 +13,6 @@ docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build $@
 # Install dependencies
 #=============================================================================
 debug "Install dependencies"
-docker-compose exec --user www app composer install
 docker-compose exec --user www app npm install
 
 # Build frontend
